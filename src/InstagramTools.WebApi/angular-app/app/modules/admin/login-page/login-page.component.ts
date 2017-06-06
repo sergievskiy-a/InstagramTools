626cdadc2af56e6dde07634cf6d84ca219cb35e1
Anton Sergievskiy <anton.sergievskiy@yandex.ua>
2017-06-06 15:14:16 +0300

    add connstring

--- src/InstagramTools.WebApi/angular-app/app/modules/admin/login-page/login-page.component.ts
+++ src/InstagramTools.WebApi/angular-app/app/modules/admin/login-page/login-page.component.ts
@@ -18,22 +18,22 @@ export class SettingsDialog {
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-  dogs = [
-    {rows: 2, name: "Mal", human: "Jeremy", age: 5},
-    {rows: 1, name: "Molly", human: "David", age: 5},
-    { rows: 1, name: "Sophie", human: "Alex", age: 8},
-    {rows: 2, name: "Taz", human: "Joey", age: '11 weeks'},
-    { rows: 1, name: "Kobe", human: "Igor", age: 5},
-    {rows: 2, name: "Porter", human: "Kara", age: 3},
-    { rows: 1, name: "Stephen", human: "Stephen", age: 8},
-    {rows: 1, name: "Cinny", human: "Jules", age: 3},
-    { rows: 1, name: "Hermes", human: "Kara", age: 3},
+  private currentDog: any;
+  private dogs = [
+    { rows: 2, name: "Mal", human: "Jeremy", age: 5 },
+    { rows: 1, name: "Molly", human: "David", age: 5 },
+    { rows: 1, name: "Sophie", human: "Alex", age: 8 },
+    { rows: 2, name: "Taz", human: "Joey", age: '11 weeks' },
+    { rows: 1, name: "Kobe", human: "Igor", age: 5 },
+    { rows: 2, name: "Porter", human: "Kara", age: 3 },
+    { rows: 1, name: "Stephen", human: "Stephen", age: 8 },
+    { rows: 1, name: "Cinny", human: "Jules", age: 3 },
+    { rows: 1, name: "Hermes", human: "Kara", age: 3 },
   ];
   @ViewChild('sidenav') sidenav: MdSidenav;
-  currentDog = {};
   isDarkTheme = false;
 
-  constructor(public dialog: MdDialog, public vcr: ViewContainerRef) {}
+  constructor(public dialog: MdDialog, public vcr: ViewContainerRef) { }
 
   openDialog() {
     const config = new MdDialogConfig();
@@ -42,6 +42,7 @@ export class LoginPageComponent {
   }
 
   showDog(dog) {
+    console.log(dog);
     this.currentDog = dog;
     this.sidenav.open();
   }
